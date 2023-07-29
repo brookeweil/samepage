e3c59dd95d5485dc4b5d46ebb9a10a659baeb7ca
Brooke Weil <brooke.marie.weil@gmail.com>
2023-07-29 12:14:42 -0400

    notes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      HELLO WORLD!
+      <h1>SamePage</h1>
+      <h2>
+        For Trivia in a loud bar, live closed-captioning, or any time you want to be on the same page.
+      </h2>
+      {/* TODO
+        - screen for when you first arrive at the page: create a room, login, or login as an editor
+        - cookie for 24h to save recent room code
+        - view screen: poll every 1-2s for room code
+        - create screen: create key and password and submit (initialize text to "Welcome" or something)
+        - edit screen: submit new text for the room key while passing pw      
+      */}
     </div>
   );
 }
